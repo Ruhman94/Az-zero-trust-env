bc92a0610bc215a3c5a3e6b4f0a2be8069ad28f7
Ruhman94 <A.mmx294@gmail.com>
2026-05-03 14:54:09 +0300

    Updated fetch config

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_container_app" "api" {
         "http://127.0.0.1:5500",
         "http://localhost:5500"
       ]
-      allowed_methods    = ["GET", "POST", "OPTIONS"]
+      allowed_methods    = ["GET", "OPTIONS"]
       allowed_headers    = ["Content-Type"] # Locked down from "*"
       max_age_in_seconds = 3600
     }
